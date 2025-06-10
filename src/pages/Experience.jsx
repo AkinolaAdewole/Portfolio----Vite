a653afaaafe5083f9e295971965386c61d97844d
Akinola Adewole <adewoleakinola24@gmail.com>
2025-06-10 23:14:59 +0100

    Experience

--- src/pages/Experience.jsx
+++ src/pages/Experience.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <div>Experience</div>
+    <div>
+                I have over two years experience.
+        <p> I am currently working for Namibra.</p>
+        <p>check my linkedIn for more details</p>
+        <p> https://www.linkedin.com/in/akinola-adewole-593460154 </p>
+    </div>
   )
 }
 
